c5c6a8fdef933050bc96b991c1449259f0672241
Harry Brundage <harry.brundage@gmail.com>
2023-09-20 13:45:29 -0400

    gitpkg

---
+++ pkg/src/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="node" />
+import Module from "module";
+/**
+ * Produce a new requirefire instance.
+ */
+declare const createRequirefire: () => {
+    (path: string): any;
+    cache: Record<string, Module>;
+};
+export type Requirefire = ReturnType<typeof createRequirefire>;
+export default createRequirefire;
